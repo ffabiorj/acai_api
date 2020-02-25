9dddb071d24b04a98b4256b7f675aad0a8ccda19
Fabio Oliveira <fabio20rj@gmail.com>
2020-02-25 15:35:53 -0300

    Added views in the project

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Acai, Pedido, Personalizacao
+from .serializers import AcaiSerializer, PedidoSerializer, PersonalizacaoSerializer
 
-# Create your views here.
+class AcaiView(viewsets.ModelViewSet):
+    queryset = Acai.objects.all()
+    serializer_class = AcaiSerializer
+
+
+class PersonalizacaoView(viewsets.ModelViewSet):
+    queryset = Personalizacao.objects.all()
+    serializer_class = PersonalizacaoSerializer
+
+
+class PedidoView(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
